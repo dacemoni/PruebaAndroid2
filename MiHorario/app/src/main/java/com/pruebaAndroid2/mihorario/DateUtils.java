b6ddab023a45a4396cd5285436abf83f85fca707
David César Moro Nieto <davidmoro96@gmail.com>
2022-11-30 10:57:59 +0100

    Cambios en el proyecto entero

---
+++ MiHorario/app/src/main/java/com/pruebaAndroid2/mihorario/DateUtils.java
@@ -0,0 +1,28 @@
+package com.pruebaAndroid2.mihorario;
+
+public class DateUtils {
+
+    /**
+     * Devuelve un dia de la semana como número.
+     * El primer dia (lunes) devuelve un 0
+     * @param dia El dia de la semana como cadena de texto
+     * @return El dia de la semana como número entero
+     */
+    public static int diaOrdinal(String dia) {
+
+        switch (dia) {
+            case "Lunes":
+                return 2;
+            case "Martes":
+                return 3;
+            case "Miércoles":
+                return 4;
+            case "Jueves":
+                return 5;
+            case "Viernes":
+                return 6;
+            default:
+                return -1;
+        }
+    }
+}
---
+++ MiHorario/app/src/main/java/com/pruebaAndroid2/mihorario/QueTocaAhoraActivity.java
@@ -0,0 +1,35 @@
+package com.pruebaAndroid2.mihorario;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static com.pruebaAndroid2.mihorario.MainActivity.asignaturas;
+
+public class QueTocaAhoraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_que_toca_ahora);
+
+        TextView tvLoQueToca = findViewById(R.id.tvLoQueToca);
+
+        int diaHoy = new GregorianCalendar().get(Calendar.DAY_OF_WEEK);
+        int horaAhora = new GregorianCalendar().get(Calendar.HOUR);
+        for (Asignatura asignatura : asignaturas) {
+            if (DateUtils.diaOrdinal(asignatura.getDia()) == diaHoy) {
+                if (Integer.parseInt(asignatura.getHora()) == horaAhora) {
+                    tvLoQueToca.setText("Te toca ir a clase de " + asignatura.getNombre());
+                    return;
+                }
+
+            }
+        }
+
+        tvLoQueToca.setText("Tiempo Libre!!!!!!!!! " + diaHoy + " " + horaAhora);
+    }
+}
