323a22e2dd34077a2610d289641d1bdcfc6c155f
David César Moro Nieto <davidmoro96@gmail.com>
2022-11-30 09:29:58 +0100

    Creación Proyecto

---
+++ pruebaAndroid2/src/main/java/com/android/pruebaAndroid2/PruebaAndroid2Application.java
@@ -0,0 +1,13 @@
+package com.android.pruebaAndroid2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAndroid2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAndroid2Application.class, args);
+	}
+
+}
